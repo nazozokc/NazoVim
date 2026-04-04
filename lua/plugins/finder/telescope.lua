6a3baa1713dfda0660c2b422c4bd5f8eef33099f
nazozokc <nazozokc@gmail.com>
2026-04-04 13:46:53 +0900

    chore: remove dead plugin files and unused telescope-ui-select

--- lua/plugins/finder/telescope.lua
+++ lua/plugins/finder/telescope.lua
@@ -1,20 +1,14 @@
 return {
-	{
-		"nvim-telescope/telescope-ui-select.nvim",
-		lazy = true,
-	},
-	{
-		"nvim-telescope/telescope.nvim",
-		cmd = { "Telescope" },
-		dependencies = { "nvim-lua/plenary.nvim" },
-		config = function()
-			require("telescope").setup({
-				pickers = {
-					find_files = {
-						hidden = true,
-					},
+	"nvim-telescope/telescope.nvim",
+	cmd = { "Telescope" },
+	dependencies = { "nvim-lua/plenary.nvim" },
+	config = function()
+		require("telescope").setup({
+			pickers = {
+				find_files = {
+					hidden = true,
 				},
-			})
-		end,
-	},
+			},
+		})
+	end,
 }
